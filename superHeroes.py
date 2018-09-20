61b065048c1371103f91b7ad14cc24f29897d233
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-09-19 23:43:22 -0700

    Arranged code

--- superHeroes.py
+++ superHeroes.py
@@ -24,25 +24,25 @@ class Ability:
 
 
     def update_attack(self, attack_strength):
-        # Update attack value
+        # ✔️ Update attack value
         attack_value = attack_strength
 
 class Hero:
     def __init__(self, name):
-        # Initialize starting values
-        #created a list to store the superheroes abilities
+        # ✔️ Initialize starting values
+        # ✔️ created a list to store the superheroes abilities
         self.abilities = list()
         self.name = name
 
 
     def add_ability(self, ability):
-        # Add ability to abilities list
-        #in order to choose a type of ability
+        # ✔️ Add ability to abilities list
+        # in order to choose a type of ability
         self.abilities.append(ability)
 
     def attack(self):
         # calculating the total amount of attacks for every ability
-        #returns the total amount of attacks
+        # returns the total amount of attacks
         total_amnt_attack = 0
         for new_attack in self.abilities:
             total_amnt_attack += new_attack.attack()
@@ -53,8 +53,6 @@ if __name__ == "__main__":
     # If you run this file from the terminal this block is executed.
     hero = Hero("Wonder Woman")
     print(hero.attack())
-
-
     ability = Ability("Divine Speed", 300)
     hero.add_ability(ability)
     print(hero.attack())
